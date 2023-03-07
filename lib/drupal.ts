d3e189b1e10d7f5d5a13ab43928d41400d7dbac0
Thomas Bloom <tb@fallonts.com>
2023-03-06 17:23:55 -0800

    test 2

--- lib/drupal.ts
+++ lib/drupal.ts
@@ -1,6 +1,16 @@
 import { DrupalClient } from "next-drupal";
+import fetch from "isomorphic-unfetch";
+
 let drupalUrl: string = process.env.NEXT_PUBLIC_DRUPAL_BASE_URL!;
 
+// Create a custom fetcher.
+const customFetcher = (url: string) => {
+  return fetch(url, {
+    // Pass in additional options. Example: agent.
+  });
+};
+
+// Pass the custom fetcher to the client.
 export const drupal = new DrupalClient(drupalUrl, {
-  previewSecret: process.env.DRUPAL_PREVIEW_SECRET,
+  fetcher: customFetcher,
 });
