032e8e2a66ed7a919268dec411422e293cb0d451
Thomas Bloom <tb@3sherpas.com>
2023-03-06 19:40:17 -0800

    test attempt 7000

--- lib/drupal.ts
+++ lib/drupal.ts
@@ -1,24 +1,7 @@
 import { DrupalClient } from "next-drupal";
-import fetch from "isomorphic-unfetch";
 
 let drupalUrl: string = process.env.NEXT_PUBLIC_DRUPAL_BASE_URL!;
 
-const customFetcher = (url: string) => {
-  return fetch(url, {
-    // Pass in additional options. Example: agent.
-  })
-    .then((response) => {
-      // Handle successful response here.
-      return response;
-    })
-    .catch((error) => {
-      // Handle any errors that occur during the fetch here.
-      console.error("fetch error");
-      console.error(error);
-    });
-};
-
-// Pass the custom fetcher to the client.
 export const drupal = new DrupalClient(drupalUrl, {
-  fetcher: customFetcher,
+  previewSecret: process.env.DRUPAL_PREVIEW_SECRET,
 });
