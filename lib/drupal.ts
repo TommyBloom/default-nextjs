0c14780a73badbddc503f2b75fe3ba2f8448bca4
Thomas Bloom <tb@fallonts.com>
2023-03-06 17:11:53 -0800

    fix type

--- lib/drupal.ts
+++ lib/drupal.ts
@@ -1,18 +1,6 @@
 import { DrupalClient } from "next-drupal";
+let drupalUrl: string = process.env.NEXT_PUBLIC_DRUPAL_BASE_URL!;
 
-const baseUrl = process.env.NEXT_PUBLIC_DRUPAL_BASE_URL;
-const previewSecret = process.env.DRUPAL_PREVIEW_SECRET;
-
-if (!baseUrl) {
-  throw new Error(
-    "NEXT_PUBLIC_DRUPAL_BASE_URL environment variable is not defined"
-  );
-}
-
-if (!previewSecret) {
-  throw new Error("DRUPAL_PREVIEW_SECRET environment variable is not defined");
-}
-
-export const drupal = new DrupalClient(baseUrl, {
-  previewSecret: previewSecret,
+export const drupal = new DrupalClient(drupalUrl, {
+  previewSecret: process.env.DRUPAL_PREVIEW_SECRET,
 });
