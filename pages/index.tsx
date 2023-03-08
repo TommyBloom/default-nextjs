477feb24c5e2c74ef69de1577f51fd62dc4243a0
Thomas Bloom <tb@3sherpas.com>
2023-03-07 21:31:14 -0800

    remove stuff

--- pages/index.tsx
+++ pages/index.tsx
@@ -41,37 +41,24 @@ export default function IndexPage({ nodes }: IndexPageProps) {
   );
 }
 
-export async function getStaticProps(
-  context: GetStaticPropsContext<ParsedUrlQuery, PreviewData>
-): Promise<GetStaticPropsResult<IndexPageProps>> {
-  try {
-    const nodes = await drupal.getResourceCollectionFromContext<DrupalNode[]>(
-      "node--project",
-      context,
-      {
-        params: {
-          "filter[status]": 1,
-          "fields[node--project]":
-            "title,path,field_teaser_image,body,uid,created",
-          include: "field_teaser_image.field_media_image,uid",
-          "fields[file--file]": "uri,url,image_style_uri",
-        },
-      }
-    );
+export async function getStaticProps(context) {
+  const nodes = await drupal.getResourceCollectionFromContext<DrupalNode[]>(
+    "node--project",
+    context,
+    {
+      params: {
+        "filter[status]": 1,
+        "fields[node--project]":
+          "title,path,field_teaser_image,body,uid,created",
+        include: "field_teaser_image.field_media_image,uid",
+        "fields[file--file]": "uri,url,image_style_uri",
+      },
+    }
+  );
 
-    return {
-      props: {
-        nodes,
-      },
-    };
-  } catch (error) {
-    console.error(error);
-    console.error("Error fetching nodes");
-    // Return an empty props object to indicate an error occurred
-    return {
-      props: {
-        nodes: [],
-      },
-    };
-  }
+  return {
+    props: {
+      nodes,
+    },
+  };
 }
