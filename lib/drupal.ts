0fc9ebf4d72429217a627d2e719f782f39e18488
Thomas Bloom <tb@3sherpas.com>
2023-03-07 20:06:44 -0800

    remove preview secret

--- lib/drupal.ts
+++ lib/drupal.ts
@@ -2,6 +2,4 @@ import { DrupalClient } from "next-drupal";
 
 let drupalUrl: string = process.env.NEXT_PUBLIC_DRUPAL_BASE_URL!;
 
-export const drupal = new DrupalClient(drupalUrl, {
-  previewSecret: process.env.DRUPAL_PREVIEW_SECRET,
-});
+export const drupal = new DrupalClient(drupalUrl, {});
