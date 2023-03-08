a739f23a13709fee6100828583117c982118a518
Thomas Bloom <tb@fallonts.com>
2023-03-08 10:27:48 -0800

    test with data

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,15 @@
 import Head from "next/head";
+import { GetStaticPropsContext, GetStaticPropsResult, PreviewData } from "next";
+import { DrupalNode } from "next-drupal";
 
-export default function IndexPage() {
+import { drupal } from "../lib/drupal";
+import { ParsedUrlQuery } from "querystring";
+
+interface IndexPageProps {
+  nodes: DrupalNode[];
+}
+
+export default function IndexPage({ nodes }: IndexPageProps) {
   return (
     <>
       <Head>
@@ -12,13 +21,45 @@ export default function IndexPage() {
       </Head>
       <div>
         <h1 className="mb-10 text-6xl font-black">Latest Articles.</h1>
+        {nodes?.length ? (
+          nodes.map((node) => (
+            <div key={node.id}>
+              <h2 className="mb-4 text-4xl font-bold">
+                <a href={node.path.alias}>{node.title}</a>
+              </h2>
+
+              <div dangerouslySetInnerHTML={{ __html: node.body.processed }} />
+              <hr className="my-20" />
+            </div>
+          ))
+        ) : (
+          <p className="py-4">No nodes found</p>
+        )}
       </div>
     </>
   );
 }
 
-export async function getStaticProps() {
+export async function getStaticProps(
+  context: GetStaticPropsContext<ParsedUrlQuery, PreviewData>
+): Promise<GetStaticPropsResult<IndexPageProps>> {
+  const nodes = await drupal.getResourceCollectionFromContext<DrupalNode[]>(
+    "node--project",
+    context,
+    {
+      params: {
+        "filter[status]": 1,
+        "fields[node--project]":
+          "title,path,field_teaser_image,body,uid,created",
+        include: "field_teaser_image.field_media_image,uid",
+        "fields[file--file]": "uri,url,image_style_uri",
+      },
+    }
+  );
+
   return {
-    props: {},
+    props: {
+      nodes,
+    },
   };
 }
