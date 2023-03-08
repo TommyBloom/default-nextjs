52fc563fa2a055c55c6dd2a84b7704703b6f9a11
Thomas Bloom <tb@3sherpas.com>
2023-03-07 21:49:13 -0800

    remove context and commented code

--- pages/index.tsx
+++ pages/index.tsx
@@ -12,26 +12,12 @@ export default function IndexPage() {
       </Head>
       <div>
         <h1 className="mb-10 text-6xl font-black">Latest Articles.</h1>
-        {/* {nodes?.length ? (
-          nodes.map((node) => (
-            <div key={node.id}>
-              <h2 className="mb-4 text-4xl font-bold">
-                <a href={node.path.alias}>{node.title}</a>
-              </h2>
-
-              <div dangerouslySetInnerHTML={{ __html: node.body.processed }} />
-              <hr className="my-20" />
-            </div>
-          ))
-        ) : (
-          <p className="py-4">No nodes found</p>
-        )} */}
       </div>
     </>
   );
 }
 
-export async function getStaticProps(context) {
+export async function getStaticProps() {
   return {
     props: {},
   };
